cd797fba28d328134bcf118ba81ee3f4aeca359e
super-archive-1419 <sophzsul@gmail.com>
2019-12-24 04:23:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let\'s talk about space";
+var topic = "Let\'s talk about Let\'s talk space";
 
 
 var conversation = `${topic}`;
